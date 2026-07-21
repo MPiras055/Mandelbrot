7f63afebbc867ca1228cc9dc5dc7c1010c7b52e5
MPiras055 <m.piras19@studenti.unipi.it>
2026-07-21 15:45:13 +0200

    untrack build

--- build/CMakeFiles/MandelbrotApp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for MandelbrotApp.
-# This may be replaced when dependencies are built.
--- build/deps/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matti/Projects/mandelbrot/deps/raylib/src
-# Build directory: /home/matti/Projects/mandelbrot/build/deps/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
